1f75f0be6304ee562fe5e368d81efcf4b3e80dbb
Frank <frankcalas0193@flpoly.org>
2017-01-24 22:41:20 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Exercism - Python
+
+This is where I will be posting all of the python problems I do through (exercism)[exercism.io]
---
+++ hello-world/hello_world.py
@@ -0,0 +1,12 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+def hello(name = False):
+    if name:
+	return ("Hello, " + name + "!")
+    elif name == False:
+	return ("Hello, World!")
+    else:
+	return ("Hello, World!")
+
